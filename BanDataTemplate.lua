5a414c082ebc8a6a3d9f7b6c6a9fe27feeeabc43
Warzone1071 <168687322+Warzone1071@users.noreply.github.com>
2025-07-28 22:19:12 -0400

    Add files via upload

---
+++ BanDataTemplate.lua
@@ -0,0 +1,9 @@
+return {
+	IsBanned = false,
+	Reason = "",
+	Duration = 0,
+	BanTime = 0, 
+	BannedBy = 0,
+}
+
+-- Note if u change the data here and you want to change something with how they ban or remove something you will have to change the banservice functions slightly
